62cc523c64ebb732e6db15eab94582cd849350cb
ana8113 <fairy_tale_ana@hotmail.com>
2015-03-17 12:58:25 +0100

    Dodata logicka kontrola u sve metoda klase Biblioteka.

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -1,6 +1,9 @@
 package biblioteka;
 
 import java.util.LinkedList;
+
+import javax.management.RuntimeErrorException;
+
 import biblioteka.interfejs.BibliotekaInterfejs;
 
 public class Biblioteka implements BibliotekaInterfejs {
@@ -9,11 +12,19 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga == null)
+			throw new RuntimeException("Knjiga ne sme biti null");
+		
+		if (knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga je vec u biblioteci");
+		
 		knjige.add(knjiga);
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if (!knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga ne postoji u biblioteci");
 		knjige.remove(knjiga);
 	}
 
@@ -22,6 +33,10 @@ public class Biblioteka implements BibliotekaInterfejs {
 			String izdavac, Autor autor) {
 		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		if (naslov == null && ISBN <= 0 && izdavac == null && autor == null)
+			throw new RuntimeException("Morate uneti neki kriterijum!");
+		
 		if (ISBN > 0) 
 			for (int i = 0; i < knjige.size(); i++) {
 				if (knjige.get(i).getISBN() == ISBN)
